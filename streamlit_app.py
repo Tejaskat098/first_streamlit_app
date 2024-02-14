11ab7912fd81f29bd495f824bd38764e7e5e40c3
Tejaskat098 <159981819+Tejaskat098@users.noreply.github.com>
2024-02-14 09:34:05 +0530

    Update streamlit_app.py
    added menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('My parents new healthy dinner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Dosa')
+streamlit.text('Vadapav')
+streamlit.text('Breakfast Menu')
